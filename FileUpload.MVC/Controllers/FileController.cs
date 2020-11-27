eb89f31c34f1748cf65948112ab48ec83ba45b65
HalimHamidov <halimjonik@gmail.com>
2020-11-27 09:52:16 +0300

    Добавьте файлы проекта.

---
+++ FileUpload.MVC/Controllers/FileController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FileUpload.MVC.Data;
+using FileUpload.MVC.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace FileUpload.MVC.Controllers
+{
+    public class FileController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        private readonly long _fileSizeLimit;
+
+        public FileController(ApplicationDbContext context, IConfiguration config)
+        {
+            this.context = context;
+            _fileSizeLimit = config.GetValue<long>("FileSizeLimit");
+        }
+        public async Task<IActionResult> Index()
+        {
+            var fileuploadViewModel = await LoadAllFiles();
+            ViewBag.Message = TempData["Message"];
+            return View(fileuploadViewModel);
+        }
+        [HttpPost]
+        public async Task<IActionResult> UploadToFileSystem(List<IFormFile> files, string description)
+        {
+
+  
+            foreach (var file in files)
+            {
+                if (file.Length < _fileSizeLimit)
+                {
+                //var filePath = Path.Combine(_config["StoredFilesPath"],
+                //    Path.GetRandomFileName());
+
+                //https://question-it.com/questions/122762/chto-takoe-parametr-_config-storedfilespath-i-gde-on-dolzhen-nahoditsja
+                    var basePath = Path.Combine(Directory.GetCurrentDirectory(),"\\StoredFilesPath\\");
+                bool basePathExists = System.IO.Directory.Exists(basePath);
+                if (!basePathExists) Directory.CreateDirectory(basePath);
+                var fileName = Path.GetFileNameWithoutExtension(file.FileName);
+                var filePath = Path.Combine(basePath, file.FileName);
+                var extension = Path.GetExtension(file.FileName);
+                var fileSize = file.Length;
+                if (!System.IO.File.Exists(filePath))
+                {
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await file.CopyToAsync(stream);
+                    }
+                    var fileModel = new FileOnFileSystemModel
+                    {
+                        CreatedOn = DateTime.UtcNow,
+                        FileType = file.ContentType,
+                        Extension = extension,
+                        Name = fileName,
+                        FileSize = fileSize,
+                        Description = description,
+                        FilePath = filePath
+                    };
+                    context.FilesOnFileSystem.Add(fileModel);
+                    context.SaveChanges();
+                }
+                }
+                else
+                {
+                    TempData["Message"] = "Multipart boundary length limit exceeded..";
+                    //throw new InvalidDataException(
+                    //    $"Multipart boundary length limit exceeded.");
+                }
+            }
+            TempData["Message"] = "File successfully uploaded to File System.";
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public async Task<IActionResult> UploadToDatabase(List<IFormFile> files, string description)
+        {
+
+            foreach (var file in files)
+            {
+                if (file.Length < _fileSizeLimit)
+                {
+                    var fileName = Path.GetFileNameWithoutExtension(file.FileName);
+                    var extension = Path.GetExtension(file.FileName);
+                    var fileSize = file.Length;
+                    var fileModel = new FileOnDatabaseModel
+                    {
+                        CreatedOn = DateTime.UtcNow,
+                        FileType = file.ContentType,
+                        Extension = extension,
+                        Name = fileName,
+                        FileSize = fileSize,
+                        Description = description
+                    };
+                    using (var dataStream = new MemoryStream())
+                    {
+                        await file.CopyToAsync(dataStream);
+                        fileModel.Data = dataStream.ToArray();
+                    }
+
+                    context.FilesOnDatabase.Add(fileModel);
+                    context.SaveChanges();
+                }
+            }
+            TempData["Message"] = "File successfully uploaded to Database";
+            return RedirectToAction("Index");
+        }
+
+        private async Task<FileUploadViewModel> LoadAllFiles()
+        {
+            var viewModel = new FileUploadViewModel();
+            viewModel.FilesOnDatabase = await context.FilesOnDatabase.ToListAsync();
+            viewModel.FilesOnFileSystem = await context.FilesOnFileSystem.ToListAsync();
+            return viewModel;
+        }
+
+        public async Task<IActionResult> DownloadFileFromDatabase(int id)
+        {
+
+            var file = await context.FilesOnDatabase.Where(x => x.Id == id).FirstOrDefaultAsync();
+            if (file == null) return null;
+            return File(file.Data, file.FileType, file.Name + file.Extension);
+        }
+        public async Task<IActionResult> DownloadFileFromFileSystem(int id)
+        {
+
+            var file = await context.FilesOnFileSystem.Where(x => x.Id == id).FirstOrDefaultAsync();
+            if (file == null) return null;
+            var memory = new MemoryStream();
+            using (var stream = new FileStream(file.FilePath, FileMode.Open))
+            {
+                await stream.CopyToAsync(memory);
+            }
+            memory.Position = 0;
+            return File(memory, file.FileType, file.Name + file.Extension);
+        }
+        public async Task<IActionResult> DeleteFileFromFileSystem(int id)
+        {
+
+            var file = await context.FilesOnFileSystem.Where(x => x.Id == id).FirstOrDefaultAsync();
+            if (file == null) return null;
+            if (System.IO.File.Exists(file.FilePath))
+            {
+                System.IO.File.Delete(file.FilePath);
+            }
+            context.FilesOnFileSystem.Remove(file);
+            context.SaveChanges();
+            TempData["Message"] = $"Removed {file.Name + file.Extension} successfully from File System.";
+            return RedirectToAction("Index");
+        }
+        public async Task<IActionResult> DeleteFileFromDatabase(int id)
+        {
+
+            var file = await context.FilesOnDatabase.Where(x => x.Id == id).FirstOrDefaultAsync();
+            context.FilesOnDatabase.Remove(file);
+            context.SaveChanges();
+            TempData["Message"] = $"Removed {file.Name + file.Extension} successfully from Database.";
+            return RedirectToAction("Index");
+        }
+    }
+}
